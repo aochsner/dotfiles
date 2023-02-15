4747d8dd99f0e07cb314e5e3187bc6887a693fd1
Andrew Ochsner <aochsner@cs.stanford.edu>
2023-02-15 10:01:31 -0600

    remove platform.. go native docker
    symlink colima default w/ dns

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,7 +1,7 @@
 # for Apple M1 macs
 #export DOCKER_BUILDKIT=0
 #export COMPOSE_DOCKER_CLI_BUILD=0
-export DOCKER_DEFAULT_PLATFORM=linux/amd64
+#export DOCKER_DEFAULT_PLATFORM=linux/amd64
 mkdir -p ~/.docker/cli-plugins
 ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
 ln -sfn $(brew --prefix)/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
