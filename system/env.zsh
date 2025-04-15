14b9727d6e49ae5535fbbd34ed94fb8a805a61a4
Andrew Ochsner <andrew.ochsner@cognizant.com>
2025-04-15 08:46:29 -0500

    fix

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,7 @@
 export EDITOR='code --wait'
+export AWS_CA_BUNDLE="$HOME/Library/cacerts.pem"
+export CURL_CA_BUNDLE="$HOME/Library/cacerts.pem"
+export HTTPLIB2_CA_CERTS="$HOME/Library/cacerts.pem"
+export REQUESTS_CA_BUNDLE="$HOME/Library/cacerts.pem"
+export SSL_CERT_FILE="$HOME/Library/cacerts.pem"
+export NODE_EXTRA_CA_CERTS="$HOME/Library/cacerts.pem"
