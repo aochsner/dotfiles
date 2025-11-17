5a9e6c40e2ba969000d929e6ee25d3724f8f4e54
Andrew Ochsner <aochsner@cs.stanford.edu>
2025-11-17 11:30:22 -0600

    bump

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -5,4 +5,4 @@
 mkdir -p ~/.docker/cli-plugins
 ln -sfn $(brew --prefix)/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
 ln -sfn $(brew --prefix)/opt/docker-buildx/bin/docker-buildx ~/.docker/cli-plugins/docker-buildx
-docker buildx install
+#docker buildx install
